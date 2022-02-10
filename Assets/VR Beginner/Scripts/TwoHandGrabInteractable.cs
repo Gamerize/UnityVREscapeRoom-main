571cb2ec4049450b4c7a81aaf3847db1d913ead5
Issac <gundam.issacwai@gmail.com>
2022-02-10 16:08:26 +0000

    initial post

---
+++ Assets/VR Beginner/Scripts/TwoHandGrabInteractable.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityEngine.XR.Interaction.Toolkit
+{
+    public class TwoHandGrabInteractable : XRGrabInteractable
+    {
+        public List<XRSimpleInteractable> secondHandGrabPoints = new List<XRSimpleInteractable>();
+
+        private XRBaseInteractor secondInteractor; //Set on Second Attach
+        private Quaternion initialSecondAttachRotation; //Required to reset second hand pivot
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            foreach(XRSimpleInteractable interactable in secondHandGrabPoints)
+            {
+                interactable.onSelectEnter.AddListener(OnSecondHandGrab);
+                interactable.onSelectExit.AddListener(OnSecondHandRelease);
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public override bool IsSelectableBy(XRBaseInteractor interactor)
+        {
+            bool isAlreadyGrabbed = selectingInteractor && !interactor.Equals(selectingInteractor);
+            return base.IsSelectableBy(interactor);
+        }
+
+        void OnSecondHandGrab(XRBaseInteractor interator)
+        {
+            Debug.Log("Second Hand Grabbed!");
+        }
+
+        void OnSecondHandRelease(XRBaseInteractor interator)
+        {
+            Debug.Log("Second Hand Released!");
+        }
+    }
+}
